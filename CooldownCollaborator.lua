ac72fff7b522da3af89ed60b7a1361816e520bb4
Nelnamara <gwilkins@local>
2026-06-11 22:46:37 -0700

    Initial commit: CooldownCollaborator skeleton

---
+++ CooldownCollaborator.lua
@@ -0,0 +1,8 @@
+-- CooldownCollaborator
+-- Group & raid cooldown coordinator for WoW Midnight (12.x)
+-- Author: Nelnamara
+
+CooldownCollaborator = {}
+local CC = CooldownCollaborator
+
+-- TODO: implement addon comm sync, shared CD timeline, party/raid auto-switch
